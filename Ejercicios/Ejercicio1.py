38bbd9699bae32d8e3562782839aba66505b7038
JuanmaCanela <juanmcanela@hotmail.com>
2023-03-27 21:06:44 -0300

    Ejercicio 1

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,7 @@
+#Implementar una función que permita obtener el valor en la sucesión de Fibonacci para un número dado
+
+def fib(num):
+    if (num==0) or (num==1):
+        return num
+    else:
+        return fib(num-1) + fib(num-2)
