dbbf09697827a4d6ea7e857eb4b863c8873fdb80
JuanmaCanela <juanmcanela@hotmail.com>
2023-05-01 13:26:50 -0300

    Ejercicios

--- TPs/TP1 - Recursividad/Ejercicio05.py
+++ TPs/TP1 - Recursividad/Ejercicio05.py
@@ -1,1 +1,15 @@
 #Desarrollar una función que permita convertir un número romano en un número decimal
+
+romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+def romano_a_decimal(nro_romano):
+    if len(nro_romano) == 0:
+        return 0
+    elif len(nro_romano) == 1:
+        return romanos[nro_romano[0]]
+    elif romanos[nro_romano[0]] < romanos[nro_romano[1]]:
+        return romanos[nro_romano[1]] - romanos[nro_romano[0]] + romano_a_decimal(nro_romano[2:])
+    else:
+        return romanos[nro_romano[0]] + romano_a_decimal(nro_romano[1:])
+    
+print(romano_a_decimal('IV'))
--- TPs/TP1 - Recursividad/Ejercicio22.py
+++ TPs/TP1 - Recursividad/Ejercicio22.py
@@ -7,3 +7,18 @@
 #a. sacar los objetos de la mochila de a uno a la vez hasta encontrar un sable de luz o que no queden más objetos en la mochila;
 #b. determinar si la mochila contiene un sable de luz y cuantos objetos fueron necesarios sacar para encontrarlo;
 #c. Utilizar un vector para representar la mochila.
+
+mochila = ['pistola', 'botella', 'vendas','sable de luz']
+
+def usar_la_fuerza(mochila, index=0, contador=0):
+    if index >= len(mochila):
+        return ('No se encontró el sable de luz')
+    elif mochila[index] == "sable de luz":
+        return ('Se encontró el sable de luz', contador+1)
+    else:
+        return usar_la_fuerza(mochila, index+1, contador+1)
+
+print(usar_la_fuerza(mochila))
+
+
+        
